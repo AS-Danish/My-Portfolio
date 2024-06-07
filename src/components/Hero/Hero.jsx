ec208f49de29282589f88d2add4a4773d1604f11
AS-Danish <asdanish123@gmail.com>
2024-06-07 17:55:40 +0530

    Made some changes

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section>
+        <div>
+            <h1>Hi, I'm Danish</h1>
+            <p>I'm a passionate full stack developer, I made many projects using React JS, Node JS, and Android. Reach out if you'd like to learn more!</p>
+            <a href="mailto:asdanish123@gmail.com">Hire me</a>
+        </div>
+        <img src="./assets/images/Myself.png" alt="" />
+    </section>
+  )
+}
