cae8392e0014b03430a9f1d7b13983896f828dd2
AS-Danish <asdanish123@gmail.com>
2024-06-08 10:23:53 +0530

    Added Particles into our Website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Hero } from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import ParticlesComponent from './components/Particles/Particles';
 
 function App() {
   return (
     <>
+    <ParticlesComponent id="particles"/>
     <Navbar/>
     <Hero/>
     </>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { useTypewriter } from 'react-simple-typewriter'
 
 export const Hero = () => {
 
@@ -12,13 +12,16 @@ export const Hero = () => {
   return (
     <section className='h-full'>
       <div className='container mx-auto px-32 h-full'>
-        <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+        <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-4 xl:pb-24'>
           <div className='text-center xl:text-left'>
-            <h1 className="pb-0 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white mb-0">Hi, It's</h1>
-            <h1 className='text-[110px] font-extrabold uppercase font-sans mt-0 py-0 text-orange-500 text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-purple-600 hover:bg-gradient-to-l'>Danish</h1>
+            <h1 className="py-0 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white mb-0">Hi, It's</h1>
+            <h1 className='text-[110px] font-extrabold uppercase font-sans mt-0 py-0 text-orange-500 text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-purple-600'>Danish</h1>
             <span className='text-4xl font-sans font-bold'>I am a </span><span className='text-4xl text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-red-500 font-extrabold'>{typeEffect}</span>
           </div>
-          <div>Photo</div>
+          <div className='flex flex-col'>
+              
+          </div>
+          <div><img src="../../../assets/images/Myself.png" alt="" /></div>
         </div>
       </div>
     </section>
