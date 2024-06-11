d58e0b93636280acf1141760d082ad165816c0a7
AS-Danish <asdanish123@gmail.com>
2024-06-11 18:08:26 +0530

    Succesfully Completed the Website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -38,7 +38,7 @@ export const Hero = () => {
             </div>
             <motion.div initial={{opacity:0, y:100}} animate={{opacity:1, y:0}} transition={{duration:1, ease: "easeIn", delay:0.2}} className='ml-5 mt-2'>
               <button className="uppercase mx-auto relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                <span className="relative px-24 py-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <span className="relative px-24 py-3 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   Download CV<FontAwesomeIcon className='ml-1' icon={faDownload} />
                 </span>
               </button>
